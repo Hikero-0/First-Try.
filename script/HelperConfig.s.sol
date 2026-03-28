763a25125caa31ce116ebeb8cb5e52127b61d88a
Hikero-0 <Hiker.lingo.0@gmail.com>
2026-03-28 21:34:21 +0800

    first commit

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.21;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mock/MockV3Aggregator.sol";
+contract HelperConfig is Script{
+
+    NetworkConfig public activeNerworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+    uint256 public constant SEPOLIA_CHAIN_ID = 11155111;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor(){
+        if (block.chainid == SEPOLIA_CHAIN_ID ) {
+            activeNerworkConfig = getSepoliaEthConfig();
+        }else {
+            activeNerworkConfig = getAnvilEthConfig();
+        }
+    }
+    function getSepoliaEthConfig() public  returns (NetworkConfig memory){
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed:0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public  returns (NetworkConfig memory){
+        if (activeNerworkConfig.priceFeed != address(0)) {
+            return activeNerworkConfig;
+        }
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS,INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed:address(mockPriceFeed)});
+        return anvilConfig;
+    }
+}
